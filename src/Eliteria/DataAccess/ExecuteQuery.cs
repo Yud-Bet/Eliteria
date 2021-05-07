02b32852cb8d631eee4b584052ec079a7c765e16
Yud-Bet <huannguyenvan3@gmail.com>
2021-05-07 09:15:01 +0700

    Add DataAccess/ExecuteQuery and Helper

---
+++ src/Eliteria/DataAccess/ExecuteQuery.cs
@@ -0,0 +1,61 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Eliteria.DataAccess
+{
+    public static class ExecuteQuery
+    {
+        public static DataTable ExecuteReader(string Query, object[] ParamList = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(Helper.ConnectionStringVal("Eliteria")))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                AddParameter(Query, command, ParamList);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+            }
+            return data;
+        }
+        public static int ExecuteNoneQuery(string Query, object[] ParamList = null)
+        {
+            using (SqlConnection connection = new SqlConnection(Helper.ConnectionStringVal("Eliteria")))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                AddParameter(Query, command, ParamList);
+                return command.ExecuteNonQuery();
+            }
+        }
+        public static object ExecuteScalar(string Query, object[] ParamList = null)
+        {
+            object data = new object();
+            using (SqlConnection connection = new SqlConnection(Helper.ConnectionStringVal("Eliteria")))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(Query, connection);
+                AddParameter(Query, command, ParamList);
+                data = command.ExecuteScalar();
+            }
+            return data;
+        }
+        private static void AddParameter(string Query, SqlCommand command, object[] ParamList)
+        {
+            if (ParamList != null)
+            {
+                string[] parameters = Query.Split(' ');
+                int i = 0;
+                for (int j = 0; j < parameters.Length; j++)
+                {
+                    if (parameters[j].Contains("@"))
+                    {
+                        command.Parameters.AddWithValue(parameters[j], ParamList[i]);
+                        i++;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ src/Eliteria/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace Eliteria
+{
+    public static class Helper
+    {
+        public static string ConnectionStringVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
