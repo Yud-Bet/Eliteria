fceae260e67c80ac0aa528f977460745456c8df4
TranDangHoangAnh <70934104+TranDangHoangAnh@users.noreply.github.com>
2021-06-04 19:16:41 +0700

    Add some view and custom button
    +Add ASavingProfileView
    +Add AddNewSavingView
    +Add Custom SearchBox

--- src/Eliteria/Views/ASavingProfileView.xaml.cs
+++ src/Eliteria/Views/ASavingProfileView.xaml.cs
@@ -13,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Eliteria
+namespace Eliteria.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ASavingProfileView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ASavingProfileView : UserControl
     {
-        public MainWindow()
+        public ASavingProfileView()
         {
             InitializeComponent();
-            DataContext = new ViewModels.MainViewModel();
         }
-
-
     }
 }
