c9158a4f43910bc600cbb2cf1934368f530764ad
Yud Bet <huan.nguyen123654@gmail.com>
2021-06-08 15:57:39 +0700

    Add UnderlineButton & TextButton
    Add TextButton.xaml & TextButton.xaml.cs
    Add UnderlineButton.xaml & Add UnderlineButton.xaml.cs

---
+++ src/Eliteria/CustomControls/TextButton.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eliteria.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for TextButton.xaml
+    /// </summary>
+    public partial class TextButton : UserControl
+    {
+        public TextButton()
+        {
+            InitializeComponent();
+        }
+
+
+        public string GroupName
+        {
+            get { return (string)GetValue(GroupNameProperty); }
+            set { SetValue(GroupNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GroupName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GroupNameProperty =
+            DependencyProperty.Register("GroupName", typeof(string), typeof(TextButton));
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextButton));
+
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(TextButton));
+
+
+    }
+}
---
+++ src/Eliteria/CustomControls/UnderlineButton.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eliteria.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for UnderlineButton.xaml
+    /// </summary>
+    public partial class UnderlineButton : UserControl
+    {
+        public UnderlineButton()
+        {
+            InitializeComponent();
+        }
+
+
+        public String Text
+        {
+            get { return (String)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(String), typeof(UnderlineButton));
+
+
+        public Brush LineColor
+        {
+            get { return (Brush)GetValue(LineColorProperty); }
+            set { SetValue(LineColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LineColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LineColorProperty =
+            DependencyProperty.Register("LineColor", typeof(Brush), typeof(UnderlineButton));
+
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(UnderlineButton));
+
+
+        public String GroupName
+        {
+            get { return (String)GetValue(GroupNameProperty); }
+            set { SetValue(GroupNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GroupName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GroupNameProperty =
+            DependencyProperty.Register("GroupName", typeof(String), typeof(UnderlineButton));
+
+
+    }
+}
