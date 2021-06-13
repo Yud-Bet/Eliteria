fdbf566a646be1d695abc8bcd396ad0fa131d77d
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-06-13 20:11:17 +0700

    Add Custom Calendar Button (Finished)

--- src/Eliteria/CustomControls/EliteriaCalendarButton.xaml.cs
+++ src/Eliteria/CustomControls/EliteriaCalendarButton.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Eliteria.CustomControls
 {
@@ -23,6 +13,58 @@ namespace Eliteria.CustomControls
         public EliteriaCalendarButton()
         {
             InitializeComponent();
+            SelectedDate = null;
+            BorderThickness = new Thickness(2);
         }
+
+
+
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Background.  This enables animation, styling, binding, etc...
+        public new static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(EliteriaCalendarButton));
+
+
+
+        public new Thickness BorderThickness
+        {
+            get { return (Thickness)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BorderThickness.  This enables animation, styling, binding, etc...
+        public new static readonly DependencyProperty BorderThicknessProperty =
+            DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(EliteriaCalendarButton));
+
+
+
+        public DateTime? SelectedDate
+        {
+            get { return (DateTime?)GetValue(SelectedDateProperty); }
+            set { SetValue(SelectedDateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedDate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedDateProperty =
+            DependencyProperty.Register("SelectedDate", typeof(DateTime?), typeof(EliteriaCalendarButton));
+
+
+
+        public DatePickerFormat SelectedDateFormat
+        {
+            get { return (DatePickerFormat)GetValue(SelectedDateFormatProperty); }
+            set { SetValue(SelectedDateFormatProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedDateFormat.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedDateFormatProperty =
+            DependencyProperty.Register("SelectedDateFormat", typeof(DatePickerFormat), typeof(EliteriaCalendarButton));
+
+
     }
 }
