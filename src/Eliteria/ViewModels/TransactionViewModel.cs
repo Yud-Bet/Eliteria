57c2f7885886789a78879b94f408657093bfb002
ThanhDan <thanhdancodoc@gmail.com>
2021-06-14 15:07:54 +0700

    edit TransactionView

--- src/Eliteria/ViewModels/TransactionViewModel.cs
+++ src/Eliteria/ViewModels/TransactionViewModel.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Eliteria.ViewModels
 {
     class TransactionViewModel : BaseViewModel
     {
+        public ICommand navigateSendMoneyCMD { get; }
+        public ICommand navigateWithdrawMoneyCMD { get; }
+        public ICommand navigateConfirmCMD { get; }
+        public ICommand navigateCancelCMD { get; }
+        public ICommand navigateCheckPrintBill { get; }
         public TransactionViewModel()
         {
-
         }
     }
 }
