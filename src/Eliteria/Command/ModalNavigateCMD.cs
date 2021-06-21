33403ee70aa85964112a12a28fed08f2b8ec4dc6
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-06-21 22:07:05 +0700

    Add modal navigation service

---
+++ src/Eliteria/Command/ModalNavigateCMD.cs
@@ -0,0 +1,16 @@
+﻿namespace Eliteria.Command
+{
+    class ModalNavigateCMD : BaseCommand
+    {
+        private readonly Services.INavigationService navigationService;
+
+        public ModalNavigateCMD(Services.INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+        public override void Execute(object parameter)
+        {
+            navigationService.Navigate();
+        }
+    }
+}
