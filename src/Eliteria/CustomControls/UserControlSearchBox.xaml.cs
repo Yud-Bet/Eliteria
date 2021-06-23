de2d4c4d7bd5b21688c93a34e3baa889960f07c2
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-06-23 17:37:45 +0700

    Fix searchbox

--- src/Eliteria/CustomControls/UserControlSearchBox.xaml.cs
+++ src/Eliteria/CustomControls/UserControlSearchBox.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Eliteria.CustomControls
 {
@@ -23,7 +12,6 @@ namespace Eliteria.CustomControls
         public UserControlSearchBox()
         {
             InitializeComponent();
-            
         }
 
 
@@ -48,16 +36,10 @@ namespace Eliteria.CustomControls
 
         public static readonly DependencyProperty SearchCommandProperty =
             DependencyProperty.Register("SearchCommand", typeof(ICommand), typeof(UserControlSearchBox));
-        
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (SearchCommand != null)
-            {
-                SearchCommand.Execute(null);
-            }
+            SearchCommand?.Execute(null);
         }
-
-     
     }
 }
