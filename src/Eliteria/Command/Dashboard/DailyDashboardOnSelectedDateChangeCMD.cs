96add5041434c8baa8091e71b3d97265c8711a68
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-06-30 12:15:29 +0700

    Fix dashboard

--- src/Eliteria/Command/Dashboard/DailyDashboardOnSelectedDateChangeCMD.cs
+++ src/Eliteria/Command/Dashboard/DailyDashboardOnSelectedDateChangeCMD.cs
@@ -73,7 +73,7 @@ namespace Eliteria.Command
                     }
                 }
 
-                if (beginIndex > endIndex)
+                if (beginIndex > endIndex && endIndex != -1)
                 {
                     message?.Execute(null);
                     return;
