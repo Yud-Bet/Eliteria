8edcc680962f8a65124a79eb4d49ef350a36285d
Yud-Bet <66458858+Yud-Bet@users.noreply.github.com>
2021-09-01 07:51:09 +0700

    Revert "Add API & DataAccess project"
    This reverts commit 5307a75c9121b0839206ce7bd6e77e916bde6a10.

---
+++ src/Eliteria/Models/DataProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eliteria.Models
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                {
+                    _ins = new DataProvider();
+                }
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        private DataProvider()
+        {
+            DB = new ELITERIAEntities(); 
+        }
+        public ELITERIAEntities DB { get; set; }
+        
+    }
+}
